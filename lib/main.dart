aa6c32f0603540f076da17ea28929bc3f748a31c
khushi18-max <khushikuwar11@gmail.com>
2022-04-18 23:25:34 +0530

    Day 2 completed

--- lib/main.dart
+++ lib/main.dart
@@ -1,21 +1,18 @@
 import 'package:flutter/material.dart';
 
+import 'home_page.dart';
+
 void main() {
-  runApp(const MyApp());
+  runApp(MyApp());
 }
 
 class MyApp extends StatelessWidget {
-  const MyApp({Key? key}) : super(key: key);
   @override
   Widget build(BuildContext context) {
+    int days = 30;
+    String name = "codepur";
     return MaterialApp(
-      home: Material(
-          child: Center(
-            child: Container(
-              child: const Text("Flutter application 1"),
-              ),
-          ),  
-        ),
-      );
+    home: Homepage(),
+    );
   }
 }
