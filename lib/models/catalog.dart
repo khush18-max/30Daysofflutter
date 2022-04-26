3016cb2f6a92e781e3d9460ad84803d343122124
khushi18-max <khushikuwar11@gmail.com>
2022-04-26 14:16:39 +0530

    day 10 completed

---
+++ lib/models/catalog.dart
@@ -0,0 +1,34 @@
+class Item {
+  final String id;
+  final String name;
+  final String desc;
+  final num price;
+  final String color;
+  final String image;
+
+  Item(
+      {required this.id,
+      required this.name,
+      required this.desc,
+      required this.price,
+      required this.color,
+      required this.image});
+}
+
+final product1 = [
+  Item(
+      id: "Codepur 1",
+      name: "iphone 12 pro",
+      desc: "Apple iphone 12 generation with three base variants",
+      price: 999,
+      color: "#33505a",
+      image: "https://m.media-amazon.com/images/I/71MHTD3uL4L.jpg")
+],
+      id: "Codepur 2",
+      name: "iphone 13 ",
+      desc: "Apple iphone 13 generation with three base variants",
+      price: 3529,
+      color: "#33506a",
+      image:
+          "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTP7Rk7Pg_6y-Lx-kp5Z5KP0l_XkizZv3fhBg&usqp=CAU")
+],
